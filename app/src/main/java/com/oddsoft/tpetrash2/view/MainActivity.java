15284aab3bfc83ed3be908bd45c58f1c77372bb9
andycheng <ccjeng@gmail.com>
2016-02-28 20:14:12 +0800

    修正功能表不正常縮合的問題

--- app/src/main/java/com/oddsoft/tpetrash2/view/MainActivity.java
+++ app/src/main/java/com/oddsoft/tpetrash2/view/MainActivity.java
@@ -18,6 +18,7 @@ import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.SpannableString;
@@ -310,8 +311,17 @@ public class MainActivity extends AppCompatActivity
         navigation.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(MenuItem menuItem) {
-                int id = menuItem.getItemId();
-                switch (id) {
+
+                //Checking if the item is in checked state or not, if not make it in checked state
+                if(menuItem.isChecked())
+                    menuItem.setChecked(false);
+                else
+                    menuItem.setChecked(true);
+
+                //Closing drawer on item click
+                drawerLayout.closeDrawers();
+
+                switch (menuItem.getItemId()) {
                     case R.id.navRecycle:
                         startActivity(new Intent(MainActivity.this, RecycleActivity.class));
                         break;
@@ -369,6 +379,28 @@ public class MainActivity extends AppCompatActivity
                 .color(Color.GRAY)
                 .sizeDp(24));
 
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar
+                ,R.string.app_name, R.string.app_name){
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                // Code here will be triggered once the drawer closes as we dont want anything to happen so we leave this blank
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                // Code here will be triggered once the drawer open as we dont want anything to happen so we leave this blank
+
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+        //Setting the actionbarToggle to drawer layout
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+        //calling sync state is necessay or else your hamburger icon wont show up
+        actionBarDrawerToggle.syncState();
 
     }
 
