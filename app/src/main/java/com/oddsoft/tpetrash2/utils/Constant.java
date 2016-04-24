e6ac9a15badb6c5fc2a90ca0a0d5f30c89301cfa
andycheng <ccjeng@gmail.com>
2016-04-24 17:44:15 +0800

    no message

--- app/src/main/java/com/oddsoft/tpetrash2/utils/Constant.java
+++ app/src/main/java/com/oddsoft/tpetrash2/utils/Constant.java
@@ -8,6 +8,7 @@ public class Constant {
     //Admob
     public static final String ADMOB_TEST_DEVICE_ID = "DF9E888CAA233DE54A7FD15B3B1A1522"; //5.0
     //public static final String ADMOB_TEST_DEVICE_ID = "1B583A23DC5020F5F1C46451CBF42359"; //4.1
+    public static final String ADMOB_UNIT_ID_MAIN = "ca-app-pub-6914084100751028/2358078814";
     public static final String ADMOB_UNIT_ID_LBS = "ca-app-pub-6914084100751028/1776974015";
     public static final String ADMOB_UNIT_ID_INFO = "ca-app-pub-6914084100751028/8561866412";
     public static final String ADMOB_UNIT_ID_QUERY = "ca-app-pub-6914084100751028/9359968412";
